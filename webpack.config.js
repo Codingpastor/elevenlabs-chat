f1bf6c6d19338ef055d549a521eac5a2e699dda6
Codingpastor <59708153+Codingpastor@users.noreply.github.com>
2025-02-24 10:20:47 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/app.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
